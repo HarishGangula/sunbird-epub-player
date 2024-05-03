a65b270a748295287faec03757b90733389473c9
Harish Kumar Gangula <harish@sanketika.in>
2024-05-03 12:22:51 +0530

    Issue #KN-984 feat: Dependency changes
    Update the dependencies and migration changes to all exports

--- projects/sunbird-epub-player/src/lib/sunbird-epub-player.module.ts
+++ projects/sunbird-epub-player/src/lib/sunbird-epub-player.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EpubPlayerComponent } from './sunbird-epub-player.component';
 import { EpubViewerComponent } from './epub-viewer/epub-viewer.component';
-import { SunbirdPlayerSdkModule  } from '@project-sunbird/sunbird-player-sdk-v9';
+import { PLAYER_CONFIG, SunbirdPlayerSdkModule  } from '@project-sunbird/sunbird-player-sdk-v9';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -15,6 +15,7 @@ import { CommonModule } from '@angular/common';
     SunbirdPlayerSdkModule,
     HttpClientModule
   ],
+  providers:[{ provide: PLAYER_CONFIG, useValue: { contentCompatibilityLevel: 5 } }],
   exports: [EpubPlayerComponent]
 })
 export class SunbirdEpubPlayerModule { }
